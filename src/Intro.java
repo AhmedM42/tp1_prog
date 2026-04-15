ad13c8ef87f7eb77c172b557c6c05c9d95fc8c00
Ahmed Mahmoud <ahmedmahmoud25m@gmail.com>
2026-04-15 17:50:58 -0400

    Fonction de base de Intro

--- src/Intro.java
+++ src/Intro.java
@@ -7,9 +7,16 @@ public class Intro {
     }
 
     public static void intro(){
-        EKO.initialiser("Glitch Runner", 31, 21);
-        EKOConsole.couleurFond(EKOCouleur.NOIR);
-        EKOConsole.couleurTexte(EKOCouleur.VERT);
-        EKOConsole.rendre(); // rafraîchit l’écran
+        EKO.initialiser("Glitch Runner : Trap Protocol", 31, 21);
+        EKOConsole.icone( "src/ImagesUtiles/GlitchRunner_icone.png");
+        EKOConsole.couleurFond(EKOCouleur.GRIS_FONCE);
+        EKOConsole.afficher(9,5, "Glitch Runner");
+        EKOConsole.afficher(9,6, "Trap protocol", EKOCouleur.VERT);
+        EKOConsole.afficher(15,10, "\u2501", EKOCouleur.VERT);
+        EKOConsole.afficher(15,10, "\uE210");
+        EKOConsole.afficher(16,10, "\u2501", EKOCouleur.VERT);
+        EKOConsole.afficher(17,10, "\u2501", EKOCouleur.VERT);
+        EKOConsole.afficher(18,10, "\uE1FB", EKOCouleur.JAUNE);
+
     }
 }
