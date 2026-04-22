dc440b8f581e33a40123e36b92e62741ed9cdd70
Ahmed Mahmoud <ahmedmahmoud25m@gmail.com>
2026-04-22 15:42:10 -0400

    AJOUT DE HACKER

--- src/Hacker.java
+++ src/Hacker.java
@@ -16,8 +16,5 @@ public class Hacker extends ObjetJeu implements Collisionnable {
 
     @Override
     public void gererCollisionAvec(ObjetJeu autre) {
-        // Logique de gestion de collision pour le Hacker
-        System.out.println("Hacker " + getNom() + " a collisionné avec " + autre.getNom());
-        // Par exemple, on pourrait réduire la santé du Hacker ou de l'autre objet
     }
 }
--- src/Intro.java
+++ src/Intro.java
@@ -1,19 +1,29 @@
 import eko.*;
-private String
 
-public class Intro extends ObjetJeu implements  Collisionnable{
+public class Intro extends ObjetJeu{
+    private String LIGNE_HORIZON = "\u2501";
+    private String ICONE_ECRAN = "\uE1FB";
 
     public Intro(String nom, int x, int y) {
         super(nom, x, y, null);
     }
 
     @Override
-    protected void mettreAJour(long deltaTemps) {
+    public void mettreAJour(long deltaTemps) {
 
     }
 
     @Override
-    protected void dessiner() {
-
+    public void dessiner() {
+        EKO.initialiser("Glitch Runner : Trap Protocol", 31, 21);
+        EKOConsole.icone( "src/ImagesUtiles/GlitchRunner_icone.png");
+        EKOConsole.couleurFond(EKOCouleur.GRIS_FONCE);
+        EKOConsole.afficher(9,5, "Glitch Runner");
+        EKOConsole.afficher(9,6, "Trap protocol", EKOCouleur.VERT);
+        EKOConsole.afficher(15,10, "\u2501", EKOCouleur.VERT);
+        EKOConsole.afficher(15,10, "\uE210");
+        EKOConsole.afficher(16,10, LIGNE_HORIZON, EKOCouleur.VERT);
+        EKOConsole.afficher(17,10, LIGNE_HORIZON, EKOCouleur.VERT);
+        EKOConsole.afficher(18,10, ICONE_ECRAN, EKOCouleur.JAUNE);
     }
 }
